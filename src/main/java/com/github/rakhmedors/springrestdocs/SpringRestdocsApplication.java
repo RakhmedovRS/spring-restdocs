bfb005798af4c18b532845f570b91a6b817c90a4
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-10-05 22:35:26 +0300

    Initial commit

---
+++ src/main/java/com/github/rakhmedors/springrestdocs/SpringRestdocsApplication.java
@@ -0,0 +1,13 @@
+package com.github.rakhmedors.springrestdocs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestdocsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestdocsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/rakhmedors/springrestdocs/SpringRestdocsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.rakhmedors.springrestdocs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestdocsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
