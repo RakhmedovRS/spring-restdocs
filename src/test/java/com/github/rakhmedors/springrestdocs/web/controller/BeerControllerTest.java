d801f1348bc6ab2476a655ee0916f8b966252088
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-22 20:58:11 +0300

    Updated BeerControllerTest

--- src/test/java/com/github/rakhmedors/springrestdocs/web/controller/BeerControllerTest.java
+++ src/test/java/com/github/rakhmedors/springrestdocs/web/controller/BeerControllerTest.java
@@ -6,11 +6,14 @@ import com.github.rakhmedors.springrestdocs.repository.BeerRepository;
 import com.github.rakhmedors.springrestdocs.web.model.BeerDTO;
 import com.github.rakhmedors.springrestdocs.web.model.BeerStyleEnum;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -19,13 +22,18 @@ import java.util.UUID;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.pathParameters;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 /**
  * @author RakhmedovRS
  * @created 10/6/2020
  */
+@ExtendWith(value = {RestDocumentationExtension.class})
+@AutoConfigureRestDocs
 @WebMvcTest(BeerController.class)
 @ComponentScan(basePackages = "com.github.rakhmedors.springrestdocs.web.mapper")
 class BeerControllerTest
@@ -44,8 +52,11 @@ class BeerControllerTest
 	{
 		given(beerRepository.findById(any())).willReturn(Optional.of(Beer.builder().build()));
 
-		mockMvc.perform(get("/api/v1/beer/" + UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
-			.andExpect(status().isOk());
+		mockMvc.perform(get("/api/v1/beer/{beerId}", UUID.randomUUID().toString()).accept(MediaType.APPLICATION_JSON))
+			.andExpect(status().isOk())
+			.andDo(document("v1/beer", pathParameters(
+				parameterWithName("beerId").description("UUID of desired beer to get")
+			)));
 	}
 
 	@Test
