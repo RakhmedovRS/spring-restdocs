43d9be3b1e339030d45bb19320bc8dcf177864f2
rakhmedovrs@gmail.com <rakhmedovrs@gmail.com>
2020-12-22 21:12:49 +0300

    Added documentation for response

--- src/test/java/com/github/rakhmedors/springrestdocs/web/controller/BeerControllerTest.java
+++ src/test/java/com/github/rakhmedors/springrestdocs/web/controller/BeerControllerTest.java
@@ -24,6 +24,8 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.BDDMockito.given;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -60,6 +62,17 @@ class BeerControllerTest
 					parameterWithName("beerId").description("UUID of desired beer to get")),
 				requestParameters(
 					parameterWithName("iscold").description("Is Beer Cold Query param")
+				),
+				responseFields(
+					fieldWithPath("id").description("Id of Beer"),
+					fieldWithPath("version").description("Version number"),
+					fieldWithPath("createdDate").description("Date Created"),
+					fieldWithPath("lastModifiedDate").description("Date Updated"),
+					fieldWithPath("beerName").description("Beer Name"),
+					fieldWithPath("beerStyle").description("Beer Style"),
+					fieldWithPath("upc").description("UPC of Beer"),
+					fieldWithPath("price").description("Price"),
+					fieldWithPath("quantityOnHand").description("Quantity On hand")
 				)
 			));
 	}
